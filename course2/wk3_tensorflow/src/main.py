f439ac835d270977eaa16f0265d3db4c056fec9a
Roman <roman.jugai@ensighten.com>
2018-08-21 17:05:22 -0700

    Added wk3_tensorflow

---
+++ course2/wk3_tensorflow/src/main.py
@@ -0,0 +1,195 @@
+import math
+import numpy as np
+import h5py
+
+import matplotlib as mpl
+mpl.use('TkAgg')
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+
+from tensorflow.python.framework import ops
+from tf_utils import load_dataset, random_mini_batches, convert_to_one_hot, predict
+
+
+def linear_function():
+    """
+    Implements a linear function:
+            Initializes W to be a random tensor of shape (4,3)
+            Initializes X to be a random tensor of shape (3,1)
+            Initializes b to be a random tensor of shape (4,1)
+    Returns:
+    result -- runs the session for Y = WX + b
+    """
+    xnp = np.random.randn(3, 1)
+    wnp = np.random.randn(4, 3)
+    bnp = np.random.randn(4, 1)
+
+    X = tf.placeholder(tf.float32, name="X")
+    W = tf.placeholder(tf.float32, name="W")
+    b = tf.placeholder(tf.float32, name="b")
+
+    Y = tf.add(tf.matmul(W, X), b)
+
+    # Create the session using tf.Session() and run it with sess.run(...) on the variable you want to calculate
+    sess = tf.Session()
+    result = sess.run(Y, feed_dict={W: wnp, X: xnp, b: bnp})
+    sess.close()
+
+    return result
+
+
+def initialize_parameters():
+    tf.set_random_seed(1)  # so that your "random" numbers match ours
+
+    W1 = tf.get_variable("W1", shape=[25, 12288], initializer=tf.contrib.layers.xavier_initializer(seed=1))
+    b1 = tf.get_variable("b1", shape=[25, 1], initializer=tf.zeros_initializer())
+    W2 = tf.get_variable("W2", shape=[12, 25], initializer=tf.contrib.layers.xavier_initializer(seed=1))
+    b2 = tf.get_variable("b2", shape=[12, 1], initializer=tf.zeros_initializer())
+    W3 = tf.get_variable("W3", shape=[6, 12], initializer=tf.contrib.layers.xavier_initializer(seed=1))
+    b3 = tf.get_variable("b3", shape=[6, 1], initializer=tf.zeros_initializer())
+
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2,
+                  "W3": W3,
+                  "b3": b3}
+
+    return parameters
+
+
+def forward_propagation(X, parameters):
+    """
+    Implements the forward propagation for the model: LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SOFTMAX
+    """
+
+    # Retrieve the parameters from the dictionary "parameters"
+    W1 = parameters['W1']
+    b1 = parameters['b1']
+    W2 = parameters['W2']
+    b2 = parameters['b2']
+    W3 = parameters['W3']
+    b3 = parameters['b3']
+
+    Z1 = tf.add(tf.matmul(W1, X), b1)   # Z1 = np.dot(W1, X) + b1
+    A1 = tf.nn.relu(Z1)                 # A1 = relu(Z1)
+    Z2 = tf.add(tf.matmul(W2, A1), b2)  # Z2 = np.dot(W2, a1) + b2
+    A2 = tf.nn.relu(Z2)                 # A2 = relu(Z2)
+    Z3 = tf.add(tf.matmul(W3, A2), b3)  # Z3 = np.dot(W3,Z2) + b3
+
+    return Z3
+
+
+def compute_cost(forward_prop, Y):
+    """
+    Arguments:
+    Z3 -- output of forward propagation (output of the last LINEAR unit), of shape (6, number of examples)
+    Y -- "true" labels vector placeholder, same shape as Z3
+
+    Returns:
+    cost - Tensor of the cost function
+    """
+
+    # to fit the tensorflow requirement, where Z3 should be m by n
+    logits = tf.transpose(forward_prop)
+    labels = tf.transpose(Y)
+
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels, logits=logits))
+
+    return cost
+
+
+def model(X_train, Y_train, X_test, Y_test, learning_rate=0.0001, num_epochs=500, minibatch_size=32, print_cost=True):
+
+    ops.reset_default_graph()  # to be able to rerun the model without overwriting tf variables
+    tf.set_random_seed(1)  # to keep consistent results
+    seed = 3  # to keep consistent results
+    (n_x, m) = X_train.shape  # (n_x: input size, m : number of examples in the train set)
+    costs = []  # To keep track of the cost
+
+    X = tf.placeholder(tf.float32, name="X")
+    Y = tf.placeholder(tf.float32, name="Y")
+
+    # Initialize parameters
+    parameters = initialize_parameters()
+    # Forward propagation: Build the forward propagation in the tensorflow graph
+    forward_prop_def = forward_propagation(X, parameters)
+    # Cost function: Add cost function to tensorflow graph
+    cost_def = compute_cost(forward_prop_def, Y)
+    # Backward propagation definition: Define the tensorflow optimizer. Use an AdamOptimizer.
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost_def)
+    # Initialize all the variables
+    init = tf.global_variables_initializer()
+
+    with tf.Session() as sess:
+        # Run the initialization
+        sess.run(init)
+        for epoch in range(num_epochs):
+            epoch_cost = 0.  # Defines a cost related to an epoch
+            num_minibatches = int(m / minibatch_size)  # number of minibatches of size minibatch_size in the train set
+            seed = seed + 1
+            minibatches = random_mini_batches(X_train, Y_train, minibatch_size, seed)
+            for minibatch in minibatches:
+                (minibatch_X, minibatch_Y) = minibatch
+                # IMPORTANT: The line that runs the graph on a minibatch.
+                # Run the session to execute the "optimizer" and the "cost", the feedict should contain a minibatch for (X,Y).
+                _, minibatch_cost = sess.run([optimizer, cost_def], feed_dict={X: minibatch_X, Y: minibatch_Y})
+                epoch_cost += minibatch_cost / num_minibatches
+            # Print the cost every epoch
+            if print_cost == True and epoch % 100 == 0:
+                print("Cost after epoch %i: %f" % (epoch, epoch_cost))
+            if print_cost == True and epoch % 5 == 0:
+                costs.append(epoch_cost)
+
+        # plot the cost
+        # plt.plot(np.squeeze(costs))
+        # plt.ylabel('cost')
+        # plt.xlabel('iterations (per tens)')
+        # plt.title("Learning rate =" + str(learning_rate))
+        # plt.show()
+
+        # lets save the parameters in a variable
+        parameters = sess.run(parameters)
+
+        # Calculate the correct predictions
+        correct_prediction = tf.equal(tf.argmax(forward_prop_def), tf.argmax(Y))
+
+        # Calculate accuracy on the test set
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+
+        print("Train Accuracy:", accuracy.eval({X: X_train, Y: Y_train}))
+        print("Test Accuracy:", accuracy.eval({X: X_test, Y: Y_test}))
+
+        return parameters
+
+
+def main():
+    print("result = " + str(linear_function()))
+
+    X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
+
+    # index = 36
+    # plt.imshow(X_train_orig[index])
+    # print("y = " + str(np.squeeze(Y_train_orig[:, index])))
+    # plt.show()
+
+    # model-part
+    # Flatten the training and test images
+    X_train_flatten = X_train_orig.reshape(X_train_orig.shape[0], -1).T
+    X_test_flatten = X_test_orig.reshape(X_test_orig.shape[0], -1).T
+    # Normalize image vectors
+    X_train = X_train_flatten / 255.
+    X_test = X_test_flatten / 255.
+
+    # Convert training and test labels to one hot matrices
+    Y_train = convert_to_one_hot(Y_train_orig, 6)
+    Y_test = convert_to_one_hot(Y_test_orig, 6)
+    #
+    learned_parameters = model(X_train, Y_train, X_test, Y_test, num_epochs=1000)
+
+    return learned_parameters
+
+
+if __name__=="__main__":
+    main()
